92cc221f22b0c261f54b76fb59b66521e57f0336
김부경 <sa01023@naver.com>
2022-08-17 21:30:07 +0900

    class

--- Class/0816 화 실습/0816 실습/가장빠른문자열.py
+++ Class/0816 화 실습/0816 실습/가장빠른문자열.py
@@ -13,9 +13,8 @@ for tc in range(t):
         for i in range(len(a)):
             if a[i:i+bn] == b:
                 cnt += 1
-                a = a[:i] + '-' + a[i+bn:]  # ssaa sa 경우 때문에 넣어줌
+                a = a[i+bn:]
                 result = 1
                 break
-
-    min_cnt = an - cnt*(bn-1)
-    print(f'#{tc + 1} {min_cnt}')  # a의 길이에서 (중복되는 단어 길이-1) 만큼 빼줌
+    min_cnt = an - cnt*(bn-1)      # a의 길이에서 (중복되는 단어 길이-1) 만큼 빼줌
+    print(f'#{tc + 1} {min_cnt}')
