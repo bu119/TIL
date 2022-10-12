a7f1fe63f36f6147aaa97497b3534be5d7730b0a
김부경 <sa01023@naver.com>
2022-10-13 01:42:41 +0900

    django

---
+++ Class/5. Django/1012 수 이론/1012 실습/데일리+실습_db_04_1_P/articles/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from .models import Article, Comment
+
+
+class ArticleForm(forms.ModelForm):
+
+    class Meta:
+        model = Article
+        # fields = '__all__'
+        # fields = ('title', 'content',)
+        exclude = ('user','like_users',)
+
+
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        # fields = '__all__'
+        exclude = ('article', 'user',)
