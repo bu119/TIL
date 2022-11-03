61f9345ae334073b48196db6bc6cb49e8428726e
김부경 <sa01023@naver.com>
2022-11-03 20:44:55 +0900

    vue

---
+++ Class/8. Vue/1102 수 이론/vue-cli/src/components/MyComponent.vue
@@ -0,0 +1,23 @@
+<template>
+<!-- 최상위 태그가 존재해야한다. -->
+  <div class="border"> 
+    <h1>이거는 내가 만든 새로운 컴포넌트다!</h1>
+    <MyComponentItem/>
+  </div>
+</template>
+
+<script>
+// 1. 불러오기
+import MyComponentItem from '@/components/MyComponentItem'
+export default {
+  name: 'MyComponent',
+  // 2. 등록하기
+  MyComponentItem,
+}
+</script>
+
+<style>
+  .border {
+    border: solid 1px black;
+  }
+</style>
