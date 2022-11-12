15947bea92a0fd230d92cbcec47b81ec4fa7b11e
김부경 <sa01023@naver.com>
2022-11-12 12:43:52 +0900

    algorithm

--- Algorithm/BAEKJOON/실버/10819_차이를최대로.py
+++ Algorithm/BAEKJOON/실버/10819_차이를최대로.py
@@ -3,22 +3,22 @@ arr = list(map(int, input().split()))
 arr.sort()
 a = []
 b = []
+ansA = ansB = 0
+
 m = n//2
-# 홀수개
-if n % 2:
+
+if n % 2:                       # 홀수개
     for i in range(m):
         a += [arr[i], arr[m + 1 + i]]
         b += [arr[m + 1 + i], arr[i]]
     a += [arr[m]]
     b += [arr[m]]
 
-# 짝수개
-else:
+
+else:                           # 짝수개
     for i in range(m):
         a += [arr[i], arr[m + i]]
         b += [arr[m + i], arr[i]]
-
-ansA = ansB = 0
 
 for j in range(n-1):
     ansA += abs(a[j] - a[j+1])
---
+++ Algorithm/BAEKJOON/실버/15649_N과M(1).py
@@ -0,0 +1,9 @@
+from itertools import permutations
+
+n, m = map(int, input().split())
+
+arr = list(range(1, n+1))
+
+for i in permutations(arr, m):
+    # print(*i)
+    print(' '.join(map(str, i)))
