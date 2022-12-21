ee2e880a80481982e30f0dc43b7b0fb067b7da96
김부경 <sa01023@naver.com>
2022-12-22 00:13:31 +0900

    react

--- Study/React/한입 크기로 잘라 먹는 리액트 (React.js)/6. React 실전 프로젝트 - 감정 일기장 만들기/emotion-diary/src/pages/Home.js
+++ Study/React/한입 크기로 잘라 먹는 리액트 (React.js)/6. React 실전 프로젝트 - 감정 일기장 만들기/emotion-diary/src/pages/Home.js
@@ -4,15 +4,22 @@ import { DiaryStateContext } from "../App";
 import MyHeader from "../components/MyHeader";
 import MyButton from "../components/MyButton";
 import DiaryList from "../components/DiaryList";
+import LineChart from "../components/LineChart";
 
 const Home = () => {
   // 일기 리스트
   const diaryList = useContext(DiaryStateContext);
 
   const [data, setData] = useState([]);
+
+  // 마지막날 저장 (날짜별 차트 보류...)
+  // const [CurLastDate, setCurLastDate] = useState('');
+
   const [curDate, setCurDate] = useState(new Date());
   // console.log(curDate)
   const headText = `${curDate.getFullYear()}년 ${curDate.getMonth() + 1}월`;
+
+  const monthData = curDate.getMonth() + 1
 
   useEffect(() => {
     // 일기가 없을 때는 할 필요없다.
@@ -38,6 +45,12 @@ const Home = () => {
       setData(
         diaryList.filter((it) => firstDay <= it.date && it.date <= lastDay)
       );
+
+      // 마지막 월 추가 (날짜별 차트 보류...)
+      // const lastDate = new Date(lastDay)
+      // console.log(lastDate.getDate())
+      // setCurLastDate(lastDate.getDate())
+
     }
   }, [diaryList, curDate]);
 
@@ -57,7 +70,7 @@ const Home = () => {
     );
   };
 
-  // 추가
+  // 달력 선택 추가
   const selectMonth = (date) => {
     const year = date.slice(0, 4)
     const month = date.slice(5, 7)
@@ -78,6 +91,7 @@ const Home = () => {
         leftChild={<MyButton text={"<"} onClick={decreaseMonth} />}
         rightChild={<MyButton text={">"} onClick={increaseMonth} />}
       />
+      <LineChart diaryList={data} month={monthData} />
       <DiaryList diaryList={data} />
     </div>
   );
