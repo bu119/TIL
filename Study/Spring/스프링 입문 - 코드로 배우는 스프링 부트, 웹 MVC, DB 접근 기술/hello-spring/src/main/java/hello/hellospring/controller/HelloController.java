ab0d41279696059ccf01f18d5a6faad7668c41a8
kimbugyeong <sa01023@naver.com>
2023-03-07 21:04:45 +0900

    spring

--- Study/Spring/스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ Study/Spring/스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller // spring framework 를 사용하기 위해
 public class HelloController {
@@ -13,4 +15,15 @@ public class HelloController {
         return "hello";
     }
 
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody // http의 body 부분에 param으로 받은 data를 직접 넣어줌 // view가 필요 없다.
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name; // param으로 받은 게 그대로 요청한 서버에 바로 전달 (name가 spring이면 "hello spring")
+    }
 }
